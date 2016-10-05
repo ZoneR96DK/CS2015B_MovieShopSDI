82dc53e8943a3e47cf0ad77458e4b0df678d9f52
ZoneR96DK <samuel.prazn@gmail.com>
2016-10-05 09:07:15 +0200

    Home page working (Paged, Search, RandomFilms)

--- MovieShopUser/Controllers/MoviesController.cs
+++ MovieShopUser/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -10,23 +11,38 @@ using System.Web.Mvc;
 using MovieShopDLL;
 using MovieShopDLL.Context;
 using MovieShopDLL.Entities;
+using PagedList;
 
 namespace MovieShopUser.Controllers
 {
     public class MoviesController : Controller
     {
+        private int NUMBER_OF_TABLE_ITEMS_PER_PAGE = 15;
         private IManager<Movie> _mm = DllFacade.GetMovieManager();
 
         // GET: Movies
-        public ActionResult Index(string searchString)
+        public ActionResult Index(string sortOrder ,string searchString, string currentFilter, int? page)
         {
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentSort = searchString;
             IEnumerable<Movie> movies = _mm.Read();
+            int pageSize = NUMBER_OF_TABLE_ITEMS_PER_PAGE;
+            int pageNumber = (page ?? 1);
             if (!String.IsNullOrEmpty(searchString))
             {
-                movies = movies.Where(x => x.Title.Contains(searchString));
-                return View(movies);
+                movies = movies.Where(x => x.Title.ToLower().Contains(searchString.ToLower()));
+                return View(movies.ToPagedList(pageNumber, pageSize));
             }
-            return View(_mm.Read());
+            
+;            return View(movies.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Movies/Details/5
