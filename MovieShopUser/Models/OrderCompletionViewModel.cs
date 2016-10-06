35ac38d0a6ebf5262cc6dcfa533a4987d758d2b1
Benc0054 <benc0054@easv365.dk>
2016-10-06 11:07:06 +0200

    Working on order completion

---
+++ MovieShopUser/Models/OrderCompletionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MovieShopDLL.Entities;
+
+namespace MovieShopUser.Models
+{
+    public class OrderCompletionViewModel
+    {
+        public IEnumerable<Customer> Email { get; set; }
+        public IEnumerable<Order> OrderCompletionForm { get; set; }
+    }
+}
