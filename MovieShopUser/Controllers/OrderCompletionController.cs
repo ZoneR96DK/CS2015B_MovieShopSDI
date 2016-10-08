585229df5a787eced0e1b236cd1b78d11cd081ad
Benc0054 <benc0054@easv365.dk>
2016-10-08 03:29:40 +0200

    Spelling corrections

---
+++ MovieShopUser/Controllers/OrderCompletionController.cs
@@ -0,0 +1,23 @@
+﻿using MovieShopDLL;
+using MovieShopDLL.Entities;
+using MovieShopUser.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MovieShopUser.Controllers
+{
+    public class OrderCompletionController : Controller
+    {
+        private IManager<Order> _om = DllFacade.GetOrderManager();
+        // GET: OrderCompletion
+        public ActionResult OrderCompletionForm()
+        {
+            OrderCompletionViewModel orderCompletionView = new OrderCompletionViewModel();
+            
+            return View(orderCompletionView);
+        }
+    }
+}
---
+++ MovieShopUser/Models/OrderCompletionViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieShopDLL.Entities;
+
+namespace MovieShopUser.Models
+{
+    public class OrderCompletionViewModel
+    {
+        //public IEnumerable<Customer> Email { get; set; }
+        public IEnumerable<Order> OrderCompletionForm { get; set; }
+    }
+}
